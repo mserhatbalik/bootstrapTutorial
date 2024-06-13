734ee252922610837ba3230e8ce9b6a1b06e23a4
serhatbalik <mserhatbalik@gmail.com>
2024-06-13 23:16:08 +0300

    IBM dashboard tutorial part 1 is completed. Started Cosmos dashboard part 1

--- bootstrapTutorial/views.py
+++ bootstrapTutorial/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # render home page
 def home(request):
-    return render(request, 'home.html')
+    # return render(request, 'ibm-home.html')
+    return render(request, 'cosmos-home.html')
